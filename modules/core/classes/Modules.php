7b6fbd46a726e11ab083fb6dd05c193a3f1f32e5
Sam Wilson <sam@samwilson.id.au>
2015-01-20 08:38:35 +0800

    Missed a new file for the modulistic system.

---
+++ modules/core/classes/Modules.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Klio;
+
+class Modules
+{
+
+    /** @var string */
+    private $baseDir;
+
+    public function __construct($baseDir)
+    {
+        $this->baseDir = $baseDir;
+    }
+
+    public function getPaths()
+    {
+        $out = array();
+        $pluginsDir = $this->baseDir . '/modules';
+        foreach (scandir($pluginsDir) as $f) {
+            if ($f[0] != '.' && is_dir($pluginsDir . '/' . $f)) {
+                $out[$f] = realpath($pluginsDir . '/' . $f);
+            }
+        }
+        return $out;
+    }
+
+    public function listDir($dir)
+    {
+        $out = array();
+        foreach ($this->getPaths() as $modDir) {
+            if (!is_dir($modDir . '/' . $dir)) {
+                continue;
+            }
+            foreach (scandir($modDir . '/' . $dir) as $d) {
+                if ($d[0] == '.') {
+                    continue;
+                }
+                $out[$d] = "$modDir/$dir/$d";
+            }
+        }
+        return $out;
+    }
+}
